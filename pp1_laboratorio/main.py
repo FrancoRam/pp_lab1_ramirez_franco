3405a2ed0b23d17ccd9347155f92f8f2a6fc10b2
FrancoRam <franconramirez1@gmail.com>
2023-06-01 02:26:23 -0300

    modificacion en el punto 17

--- pp1_laboratorio/main.py
+++ pp1_laboratorio/main.py
@@ -1,4 +1,13 @@
 from desarrollo_funciones import *
 
-dream_team_app(lista_jugadores)
-#bonus_jugadores(lista_jugadores)
+#dream_team_app(lista_jugadores)
+
+def mostrar_mayor_cantidad_de_logros(lista:list):
+    
+    #print(cadena)
+    for jugador in lista:
+
+        if re.search(r'\d+', jugador['logros'][0]):
+            cadena1 = re.findall(r'\d+', jugador['logros'][0])
+        print(cadena1)
+mostrar_mayor_cantidad_de_logros(lista_jugadores)
